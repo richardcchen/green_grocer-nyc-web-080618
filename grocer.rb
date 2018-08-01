144f2c794377083aa5744c99e4e0410744e26545
Richard Chen <richard.c.chen@gmail.com>
2018-08-01 19:28:39 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -21,6 +21,7 @@ end
 
 def apply_coupons(cart, coupons)
   # code here
+=begin
   couponlength = coupons.length
   couponchecker = {}
   coupons.each do |indcoupon|
@@ -59,7 +60,22 @@ def apply_coupons(cart, coupons)
   end
   #binding.pry
   cart
+=end
+  coupons.each do |coupon|
+    name = coupon[:item]
+    if cart[name] && cart[name][:count] >= coupon[:num]
+      if cart["#{name} W/COUPON"]
+        cart["#{name} W/COUPON"][:count] += 1
+      else
+        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
+        cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
+      end
+      cart[name][:count] -= coupon[:num]
+    end
+  end
+  cart
 end
+
 
 def apply_clearance(cart)
   # code here
